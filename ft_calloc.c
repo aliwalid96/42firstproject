b13faf9fa72ef82992316037b95b2e2e81669130
Ali Muhsen <amuhsen@c1r7s4.42amman.com>
2024-10-08 10:34:08 +0300

    solved all

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuhsen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 03:04:33 by amuhsen           #+#    #+#             */
+/*   Updated: 2024/10/06 22:54:36 by amuhsen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*ptr;
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	else
+		return (ft_bzero(ptr, nmemb * size));
+}
